4b0783c7cefd30db112589f0e553afb87a5f4db2
1348079150@qq.com <1348079150@qq.com>
2020-01-07 20:45:55 +0800

    aaaa

--- src/sssss.java
+++ src/sssss.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author anchen@citinetnc.com
  * @date 2019/11/29 11:05
@@ -5,7 +10,10 @@
 public class sssss {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+       Map<String, Object> objectMap = new HashMap<>();
+       objectMap.put("1","332");
+       objectMap.put("2","33");
+        System.out.println(objectMap);
     }
 
 }
